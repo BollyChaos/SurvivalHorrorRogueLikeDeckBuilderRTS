a75d8e8cb709736d77c129c245c3beebd5a2323c
adrianUrjc <133696658+adrianUrjc@users.noreply.github.com>
2025-10-07 16:46:45 +0200

    v(0.1.0)
    Añadidas la interfaz de las cartas y la iluminación

--- SHRDB/Assets/_Scripts/Cards/CardObject.cs
+++ SHRDB/Assets/_Scripts/Cards/CardObject.cs
@@ -1,19 +1,69 @@
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CardObject : MonoBehaviour
 {
+
     [SerializeField]
     public
     CardsSO card;
-    private void BuildCard()
+    [SerializeField]
+    Color AttackColor;
+    [SerializeField]
+    Color DefenseColor;
+    [SerializeField]
+    Color UtilityColor;
+    //Aqui se pueden añadir materiales distintos para lueog las rarezas
+
+    [ContextMenu("BuildCard")]
+    public void BuildCard()
     {
         //basado en las propiedades de la carta en un prefab vacio mete los valores del so
+        //1. Ver tipo 
+        Image cardSprite=null;
+        foreach (Transform child in transform)
+        {
+            cardSprite = child.GetComponentInChildren<Image>();
+            if (cardSprite != null)
+                break;
+        }
+
+        
+        if (card != null)
+            switch (card.cardType)
+            {
+                case CardType.Attack:
+                    cardSprite.color = AttackColor;
+                    break;
+                case CardType.Defense:
+                    cardSprite.color = DefenseColor;
+                    break;
+                case CardType.Utility:
+                    cardSprite.color = UtilityColor;
+                    break;
+            }
+        //Los hijos cardtitle y carddesccription contienen el titulo y la descripcion respectivamente
+        Transform cardChild = transform.Find("CardTitle");
+        if (cardChild != null)
+        {
+            var comp = cardChild.GetComponent<TextMeshProUGUI>();
+            comp.text=card.CardName;
+        }
+        cardChild = transform.Find("CardDescription");
+        if (cardChild != null)
+        {
+            var comp = cardChild.GetComponent<TextMeshProUGUI>();
+            comp.text=card.Description;
+        }
+
+
     }
-   public void UseCard()
+    public void UseCard()
     {
         Debug.Log($"Usando la carta:{card.CardName}");
     }
-   private void Discard()
+    private void Discard()
     {
 
     }
