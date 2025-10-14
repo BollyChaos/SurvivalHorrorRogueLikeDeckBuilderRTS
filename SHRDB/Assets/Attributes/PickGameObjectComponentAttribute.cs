d2b4ed7c5d144c4ff98111d142a7b3e5b0eb1f70
adrianUrjc <133696658+adrianUrjc@users.noreply.github.com>
2025-10-14 13:27:02 +0200

    MainLimpio

--- SHRDB/Assets/Attributes/PickGameObjectComponentAttribute.cs
+++
@@ -1,8 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PickGameObjectComponentAttribute : PropertyAttribute
-{
-    // Sin contenido por ahora, solo para marcar propiedades
-}
