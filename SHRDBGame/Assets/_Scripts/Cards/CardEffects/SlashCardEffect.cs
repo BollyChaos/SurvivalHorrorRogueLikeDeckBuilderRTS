9fec1ca00be3c2e834dfe4e611714fc3855a1532
adrianUrjc <133696658+adrianUrjc@users.noreply.github.com>
2025-10-27 18:29:50 +0100

    v(1.1.5)
    Metido ejemplo de efecto de cartas

---
+++ SHRDBGame/Assets/_Scripts/Cards/CardEffects/SlashCardEffect.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlashCardEffect : MonoBehaviour, ICardEffect
+{
+    //poner atributos
+    [SerializeField]
+    float tamanoEspada = 5f;
+    public void UseCard()
+    {
+       
+    }
+}
--- SHRDBGame/Assets/_Scripts/Cards/CardLogicHandler.cs
+++ SHRDBGame/Assets/_Scripts/Cards/CardLogicHandler.cs
@@ -5,39 +5,28 @@ using UnityEngine;
 
 public class CardLogicHandler : MonoBehaviour
 {
+
     //Usar un diccionario donde hay un string nombre de la carta, y una referencia a la funcion que hace, ha de ser puesto a mano
     //Un diccionario no sale en el inspector
     //Dictionary<string,>
-    Dictionary<string, Action<CardObject>> cardEffects=new Dictionary<string, Action<CardObject>>();
-    void Awake()
-    {
-        //se añade el nombre de la carta y el comportamiento(la funcion que dispara la corrutina)
-        cardEffects.Add("hola", DefaultCardBehaviourFunction);
-    }
+[SerializeField]    List<GameObject> cardsEffects = new List<GameObject>();
+   
     internal void UseCard(CardObject cardObj)
     {
         //buscar la carta en el diccionario, si no est� usar default
         //gestionar l�gica de usar una carta(duraci�n, si es la misma no interrumpir, el manejo de los usos de la carta, descartarla
         Debug.Log($"[CardLogicHandler]Me ha llegado la carta {cardObj.card.CardName}");
-        if (cardEffects.ContainsKey(cardObj.card.CardName))
+        GameObject cardToUse = cardsEffects.Find(ce => name == cardObj.card.CardName);//busca el prefab de la carta
+        if (cardToUse!=null)//si lo encuentra le va a decir que haga su efecto de carta
         {
-            cardEffects[cardObj.card.CardName].Invoke(cardObj);
+            //instanciar, usar y destruir(ellos mismos al acabar)
+            cardToUse.GetComponent<ICardEffect>().UseCard();
+            cardObj.UsingCard = false;
         }
+       
         
     }
-    IEnumerator DefaultCardBehaviour(CardObject cardObj)
-    {
-        
-        yield return new WaitForSeconds(3f);
-
-        cardObj.UsingCard = false;
-        //GameObject.Find("Player").GetComponent<PlayerStats>()
-    }
-    void DefaultCardBehaviourFunction(CardObject cardObject)
-    {
-        StartCoroutine(DefaultCardBehaviour(cardObject));
-        
-    }
+    
 
     
 }
