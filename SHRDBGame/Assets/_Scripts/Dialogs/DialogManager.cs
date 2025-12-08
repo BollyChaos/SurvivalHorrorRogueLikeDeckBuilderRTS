a10bc9994eb7069188749c89cd7d4bbba9cd8448
adrianUrjc <133696658+adrianUrjc@users.noreply.github.com>
2025-12-08 01:26:42 +0100

    v(2.11.1)
    Los dialogos bloquean a los enemigos

--- SHRDBGame/Assets/_Scripts/Dialogs/DialogManager.cs
+++ SHRDBGame/Assets/_Scripts/Dialogs/DialogManager.cs
@@ -39,6 +39,7 @@ public class DialogManager : ASingleton<DialogManager>, IManager
                 UIManager.Instance.HideJoystick();  
             }
             UIManager.Instance.HideCardsDialog();
+            EnemyManager.Instance.OnDialogEnemies();
             Debug.Log("[DialogManager] Iniciando dialogo: "+dialogName);
             RunningDialog = StartCoroutine(PlayDialog(dialogName));
         }
