d173fa96021b5841596b92ebf912d710b88ffd0e
Jositopk <josepastorgonz@gmail.com>
2025-12-08 14:38:13 +0100

    v2.11.9.1
    Ahora los padres no te hacen ruido sin sentido cuando pasas la noche

--- SHRDBGame/Assets/_Scripts/PatronState/Padre/PadreBehaviour.cs
+++ SHRDBGame/Assets/_Scripts/PatronState/Padre/PadreBehaviour.cs
@@ -253,7 +253,7 @@ public class PadreBehaviour : MonoBehaviour
             {
                 rooms.Add(child.gameObject);
             }
-            currentRoom = rooms[2];
+            currentRoom = rooms[4];
         }
         else
         {
